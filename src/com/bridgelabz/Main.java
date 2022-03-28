365a7214030cba069a41b08e5eb84a36ce839e50
Chougulenik <nikitachougule47@gmail.com>
2022-03-28 10:31:25 -0400

    added

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		DeckOfCard cards = new DeckOfCard();
+		cards.deckOfCards();
+		cards.noOfPlayers();
+		cards.sequenceOfPlay(2);
+
+	}
+
+}
